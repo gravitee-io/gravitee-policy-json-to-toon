31d1b4d75db687bbffee01c686b26dab7b8dd37b
ThibaudAv <thibaud.avenier@gmail.com>
2026-01-21 16:09:59 +0100

    fix: add license headers to policy and test files

--- src/main/java/io/gravitee/policy/jsontotoon/JsonToToonPolicy.java
+++ src/main/java/io/gravitee/policy/jsontotoon/JsonToToonPolicy.java
@@ -1,12 +1,24 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.gravitee.policy.jsontotoon;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.toonformat.jtoon.DecodeOptions;
-import dev.toonformat.jtoon.Delimiter;
 import dev.toonformat.jtoon.EncodeOptions;
 import dev.toonformat.jtoon.JToon;
-import dev.toonformat.jtoon.KeyFolding;
-import dev.toonformat.jtoon.PathExpansion;
 import io.gravitee.common.http.HttpStatusCode;
 import io.gravitee.gateway.api.buffer.Buffer;
 import io.gravitee.gateway.api.http.HttpHeaderNames;
